954040f98b58d67cf33889d0a5918f47ee8f01e7
delucca <delucca@protonmail.com>
2021-08-19 21:36:47 -0300

    feat(warehouse): adds team cadence progress

--- warehouse/models/data_marts/okr/facts/fct__team_cadence_progress.sql
+++ warehouse/models/data_marts/okr/facts/fct__team_cadence_progress.sql
@@ -11,27 +11,22 @@ with
     select * from {{ ref('dim__cycle') }}
   ),
 
-  key_result_latest_check_in as (
-    select * from {{ ref('fct__key_result_latest_check_in') }}
+  cycle_progress as (
+    select * from {{ ref('fct__cycle_progress') }}
   ),
 
-  key_result_check_in as (
-    select * from {{ ref('dim__key_result_check_in') }}
-  ),
-
-  team_key_result_progress as (
+  team_cycle_progress as (
     select
       team.id as team_id,
       cycle.id as cycle_id,
       cycle.date_start as cycle_date_start,
       cycle.cadence as cadence,
-      {{ calculate_progress('key_result_check_in', 'key_result') }} as progress,
-      key_result_check_in.created_at as date
+      cycle_progress.progress,
+      cycle_progress.date
       from team
       left join key_result on team.id = key_result.team_id
-      left join key_result_latest_check_in on key_result.id = key_result_latest_check_in.key_result_id
-      left join key_result_check_in on key_result_latest_check_in.key_result_check_in_id = key_result_check_in.id
       left join cycle on key_result.cycle_id = cycle.id
+      left join cycle_progress on cycle.id = cycle_progress.cycle_id
   ),
 
   team_progress_in_cycle as (
@@ -42,7 +37,7 @@ with
       cadence,
       avg(progress) as progress,
       max(date) as date
-      from team_key_result_progress
+      from team_cycle_progress
       group by team_id, cycle_id, cycle_date_start, cadence
   ),
 

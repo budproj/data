4b9a3781c8ed6e60a17f001d3d6f143245371b81
delucca <delucca@protonmail.com>
2021-10-25 18:54:49 -0300

    feat(user): adds average acceses per week

--- warehouse/models/data_marts/okr/facts/fct__user_accesses.sql
+++ warehouse/models/data_marts/okr/facts/fct__user_accesses.sql
@@ -5,11 +5,24 @@ with
 
   final as (
     select
-      user_id,
-      id as amplitude_event_id,
-      event_time as access_time
-      from amplitude_event
-      where event_type = 'PageView'
+      a1.user_id,
+      a1.session_id,
+      a2.id as amplitude_event_id,
+      a2.event_time as access_time
+      from amplitude_event a1
+      left outer join lateral (
+        select
+          *
+          from amplitude_event a2
+          where
+            a2.event_type = 'PageView' and
+            a1.user_id = a2.user_id and
+            a1.session_id = a2.session_id
+          order by a2.event_time
+          limit 1
+      ) as a2 on true
+      where a1.event_type = 'PageView'
+      group by a1.user_id, a1.session_id, a2.id, a2.event_time
   )
 
 select * from final

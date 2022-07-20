2939e4a1a7f87c39530f96b52a53dcf8864a9d08
igoromote <31250794+igoromote@users.noreply.github.com>
2022-07-20 17:20:36 -0300

    feat(fct): create fact table with all days cross join users is active (#17)

---
+++ warehouse/models/data_marts/okr/facts/fct__user_is_active_by_day.sql
@@ -0,0 +1,55 @@
+with
+  amplitude_events as (
+    select * from {{ ref('dim__amplitude_event') }}
+  ),
+
+  users as (
+    select * from {{ ref('dim__user') }}
+  ),
+
+  first_event as (
+    select
+      *
+      from amplitude_events
+      order by event_time asc
+      limit 1
+  ),
+
+  days_since_first_event as (
+    select
+      {{ days_in_range('event_time', 'current_date') }} as days_since_first_event
+    from first_event
+  ),
+
+  day_series as (
+    select
+      generate_series(0, days_since_first_event) as day_delta
+    from days_since_first_event
+  ),
+
+  days as (
+    select
+      date_trunc('day', event_time + interval '1 day' * day_delta) as day
+    from day_series
+    left join first_event on 1=1
+  ),
+
+  is_user_active as (
+    select
+      distinct
+      date_trunc('day', event_time) as day,
+      user_id
+    from amplitude_events
+  ),
+
+  final as (
+    select
+      days.day as day,
+      users.id as user_id,
+      is_user_active.user_id is not null as is_active
+    from days
+    cross join users
+    left join is_user_active on is_user_active.day = days.day and users.id = is_user_active.user_id
+  )
+
+select * from final

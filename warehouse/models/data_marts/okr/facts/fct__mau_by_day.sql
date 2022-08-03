ad2cf2b520796d4fd76eaa1a0abc15f1c4fa3e82
igoromote <31250794+igoromote@users.noreply.github.com>
2022-08-03 09:59:25 -0300

    fix(fct): fix ambiguous reference to user_id (#20)

--- warehouse/models/data_marts/okr/facts/fct__mau_by_day.sql
+++ warehouse/models/data_marts/okr/facts/fct__mau_by_day.sql
@@ -12,19 +12,19 @@ final as (
     ac.user_type,
     ac.flag_owns_team,
     fcm.company_id,
-    count(distinct(user_id)) as unique_users,
+    count(distinct(ac.user_id)) as unique_users,
     count(
       distinct(
         case
-          when ac.is_active is true then user_id
+          when ac.is_active is true then ac.user_id
         end
       )
     ) as unique_active_users
   from
     calendar c
-    join dm_okr.fct__user_is_active_by_day ac on ac.day >= c.day - interval '30' day
+    join {{ ref('fct__user_is_active_by_day') }} ac on ac.day >= c.day - interval '30' day
     and ac.day <= c.day
-    join dm_okr.fct__company_members fcm on ac.user_id = fcm.user_id
+    join {{ ref('fct__company_members') }} fcm on ac.user_id = fcm.user_id
   group by
     c.day,
     ac.user_type,

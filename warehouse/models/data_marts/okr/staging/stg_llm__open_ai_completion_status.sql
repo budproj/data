0f36c3a409e2f0d15fa2fcfe0cb91827de6aff71
igoromote <31250794+igoromote@users.noreply.github.com>
2023-07-20 11:05:16 -0300

    Feat/update llm schema (#45)
    * fix(schema): update schema columns names and source
    * fix(dim): casting columns and add messages column
    * fix(stg): casting llm columns

--- warehouse/models/data_marts/okr/staging/stg_llm__open_ai_completion_status.sql
+++ warehouse/models/data_marts/okr/staging/stg_llm__open_ai_completion_status.sql
@@ -2,30 +2,33 @@ with src_dim__llm_openai as (
     select
         *
     from
-        {{ source('conformed', 'llm__llm_openaigeneration_subset_columns') }}
+        {{ source('conformed', 'llm__openaicompletion') }}
 ),
 
 final as (
     select
-        SPLIT_PART(id, '.', 3) :: text as id,
-        input,
+        id :: text,
+        input :: jsonb,
+        messages :: jsonb,
         model :: text,
         action :: text,
         entity :: text,
         output :: text,
         status :: text,
-        request,
-        response,
+        request :: jsonb,
+        response :: jsonb,
         createdat :: timestamp as created_at,
-        referenceid as reference_id,
+        referenceid :: text as reference_id,
         requestedat :: timestamp as requested_at,
         respondedat :: timestamp as responded_at,
-        producedtokens as produced_tokens,
-        consumedtokens as consumed_tokens,
-        totaltokens as total_tokens,
-        requesteruserid as requester_user_id,
-        requesterteamid as requester_team_id,
-        requestercompanyid as requester_company_id
+        promptTokens :: integer as prompt_tokens,
+        completionTokens :: integer as completion_tokens,
+        estimatedCompletionTokens :: integer as estimated_completion_tokens,
+        estimatedPromptTokens :: integer as estimated_prompt_tokens,
+        totaltokens :: integer as total_tokens,
+        requesteruserid :: uuid as requester_user_id,
+        requesterteamid :: uuid as requester_team_id,
+        requestercompanyid :: uuid as requester_company_id
     from
         src_dim__llm_openai
 )

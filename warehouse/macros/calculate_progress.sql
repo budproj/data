6e1d6a9b6d9397a9b856eeeddb6ea3e42ca55828
Igor Mateus Omote <igor.omote@gmail.com>
2023-11-08 15:08:31 -0300

    fix(macro): fix macro that calculates progress

--- warehouse/macros/calculate_progress.sql
+++ warehouse/macros/calculate_progress.sql
@@ -1,11 +1,20 @@
-{% macro calculate_progress(check_in_alias, kr_alias) -%}
-  greatest(
-    least(
-      (({{ check_in_alias }}.value - {{ kr_alias }}.initial_value)) /
-      coalesce(
-        nullif(({{ kr_alias }}.goal - {{ kr_alias }}.initial_value), 0),
-        1
-      ), 100
-    ), 0
-  )
+{% macro calculate_progress(check_in_alias, kr_alias, mode) -%}
+  case 
+    when  
+      mode = 'ASCENDING' and check_in_alias >= kr_alias 
+    then 100
+    when 
+      mode = 'DESCENDING' and check_in_alias <= kr_alias 
+    then 100
+    else
+      greatest(
+        least(
+          (({{ check_in_alias }}.value - {{ kr_alias }}.initial_value)) /
+          coalesce(
+            nullif(({{ kr_alias }}.goal - {{ kr_alias }}.initial_value), 0),
+            1
+          ), 100
+        ), 0
+      )
+    end
 {%- endmacro %}

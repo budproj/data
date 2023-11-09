85f2a086dbb549fed3457cfd26d33674151592eb
igoromote <31250794+igoromote@users.noreply.github.com>
2023-11-09 13:37:50 -0300

    Fix/calculate progress (#55)
    * fix(macro): fix macro that calculates progress
    * fix(macro): use alias as table not column
    * fix(macro): use type instead of mode
    * fix(macro): last scenario

--- warehouse/macros/calculate_progress.sql
+++ warehouse/macros/calculate_progress.sql
@@ -1,11 +1,23 @@
 {% macro calculate_progress(check_in_alias, kr_alias) -%}
-  greatest(
-    least(
-      (({{ check_in_alias }}.value - {{ kr_alias }}.initial_value)) /
-      coalesce(
-        nullif(({{ kr_alias }}.goal - {{ kr_alias }}.initial_value), 0),
-        1
-      ), 100
-    ), 0
-  )
+  case 
+    when
+      {{ check_in_alias }}.value = {{ kr_alias }}.goal
+    then 100
+    when
+      {{ kr_alias }}.type = 'ASCENDING' and {{ check_in_alias }}.value >= {{ kr_alias }}.goal
+    then 100
+    when 
+      {{ kr_alias }}.type = 'DESCENDING' and {{ check_in_alias }}.value <= {{ kr_alias }}.goal
+    then 0
+    else
+      greatest(
+        least(
+          (({{ check_in_alias }}.value - {{ kr_alias }}.initial_value)) /
+          coalesce(
+            nullif(({{ kr_alias }}.goal - {{ kr_alias }}.initial_value), 0),
+            1
+          ), 100
+        ), 0
+      )
+    end
 {%- endmacro %}
